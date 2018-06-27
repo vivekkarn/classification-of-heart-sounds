8c77aebb4ea3c8810b97f4844f577654221c5194
vivekkarn <mailervivek@gmail.com>
2018-06-27 21:53:04 +0545

    audiokeras

--- classifying-heart-sounds-using-tf.py
+++ classifying-heart-sounds-using-tf.py
@@ -23,13 +23,9 @@ def decodeFolder(category):
 def extract_feature(file_name):
 	print("Extracting "+file_name+" ...")
 	X, sample_rate = librosa.load(file_name)
-	stft = np.abs(librosa.stft(X))
-	mfccs = np.mean(librosa.feature.mfcc(y=X, sr=sample_rate, n_mfcc=40).T,axis=0)
-	chroma = np.mean(librosa.feature.chroma_stft(S=stft, sr=sample_rate).T,axis=0)
-	mel = np.mean(librosa.feature.melspectrogram(X, sr=sample_rate).T,axis=0)
-	contrast = np.mean(librosa.feature.spectral_contrast(S=stft, sr=sample_rate).T,axis=0)
-	tonnetz = np.mean(librosa.feature.tonnetz(y=librosa.effects.harmonic(X),sr=sample_rate).T,axis=0)
-	return np.hstack((mfccs,chroma,mel,contrast,tonnetz))
+	mfcc = librosa.feature.mfcc(y=X, sr=sample_rate, n_mfcc = 20).T.flatten()[:, np.newaxis].T
+	print(len(mfcc))
+	return np.array(mfcc)
 
 #train data
 normal_sounds = decodeFolder("normal")
